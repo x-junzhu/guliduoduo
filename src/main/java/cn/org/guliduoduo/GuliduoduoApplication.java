df06db3dd4016e907fe95b6bd55ff91988819e4c
x-junzhu <x_junzhu@163.com>
2022-05-10 09:19:57 +0800

    Initial Project

---
+++ src/main/java/cn/org/guliduoduo/GuliduoduoApplication.java
@@ -0,0 +1,13 @@
+package cn.org.guliduoduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuliduoduoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuliduoduoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/org/guliduoduo/GuliduoduoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.org.guliduoduo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliduoduoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
