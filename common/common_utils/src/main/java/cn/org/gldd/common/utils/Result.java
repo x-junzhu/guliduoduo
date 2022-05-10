954685a232f55f925c0ee897bd682203671eb13f
x-junzhu <x_junzhu@163.com>
2022-05-10 15:59:00 +0800

    master修改Result:删除success方法

--- common/common_utils/src/main/java/cn/org/gldd/common/utils/Result.java
+++ common/common_utils/src/main/java/cn/org/gldd/common/utils/Result.java
@@ -36,13 +36,6 @@ public class Result {
    }
 
    // 链式返回结果
-   public Result success(Boolean success){
-      this.setSuccess(success);
-      this.setMessage(ResultCode.SUCCESS_MSG);
-      this.setCode(ResultCode.SUCCESS);
-      return this;
-   }
-
    public Result message(String message){
       this.setMessage(message);
       return this;
