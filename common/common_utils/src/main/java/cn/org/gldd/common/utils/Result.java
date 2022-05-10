dd102bb19d9bdbb6eca68d1edcb70b279ae50688
x-junzhu <x_junzhu@163.com>
2022-05-10 15:56:45 +0800

    修改Result.ok and error方法

--- common/common_utils/src/main/java/cn/org/gldd/common/utils/Result.java
+++ common/common_utils/src/main/java/cn/org/gldd/common/utils/Result.java
@@ -43,6 +43,15 @@ public class Result {
       return this;
    }
 
+   public static Result ok(){
+      return new Result(true, ResultCode.SUCCESS, ResultCode.SUCCESS_MSG);
+   }
+
+   public static Result error(){
+      return new Result(false, ResultCode.ERROR, ResultCode.FAILURE_MSG);
+   }
+
+
    public Result message(String message){
       this.setMessage(message);
       return this;
